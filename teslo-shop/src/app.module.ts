ca226ef4a79c544d30a3dd32b7ebc7d0c13c724a
Elias Garcia Carvajal <ega.carvajal@gmail.com>
2024-04-25 18:37:18 -0400

    Terminando Seccion 10: TypeORM - Postgres

--- teslo-shop/src/app.module.ts
+++ teslo-shop/src/app.module.ts
@@ -1,6 +1,26 @@
 import { Module } from "@nestjs/common";
+import { ConfigModule } from "@nestjs/config";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { ProductsModule } from './products/products.module';
+import { CommonModule } from './common/common.module';
 
 @Module({
-  imports: []
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      database: process.env.DB_NAME,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      //Para cargar automaticamente entidades que vamos definiendo
+      autoLoadEntities: true,
+      //Sincroniza automaticamente los cambios en la bd
+      synchronize: true
+    }),
+    ProductsModule,
+    CommonModule
+  ]
 })
 export class AppModule { }
---
+++ teslo-shop/src/products/dto/create-product.dto.ts
@@ -0,0 +1,42 @@
+import { IsArray, IsIn, IsInt, IsNumber, IsOptional, IsPositive, IsString, MinLength } from "class-validator";
+
+export class CreateProductDto {
+
+  @IsString()
+  @MinLength(1)
+  title: string;
+
+  @IsNumber()
+  @IsPositive()
+  @IsOptional()
+  price?: number;
+
+  @IsString()
+  @MinLength(1)
+  @IsOptional()
+  description?: string;
+
+  @IsString()
+  @MinLength(1)
+  @IsOptional()
+  slug?: string;
+
+  @IsInt()
+  @IsPositive()
+  @IsOptional()
+  stock?: number;
+
+  @IsString({ each: true })
+  @IsArray()
+  sizes: string[];
+
+  @IsString()
+  @IsIn(['men', 'women', 'unisex', 'other'])
+  @MinLength(1)
+  gender: string;
+
+  @IsString({ each: true })
+  @IsArray()
+  @IsOptional()
+  tags: string[];
+}
